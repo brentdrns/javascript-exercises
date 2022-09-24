b467b32c1551c3bb5ee56827c1e56d55a695fe2d
Brent Rowley <brentdrns@gmail.com>
2022-09-24 14:43:04 -0700

    .

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	c = a + b;
+  return c;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	c = a - b;
+  return c;
 };
 
-const sum = function() {
-	
+const sum = function(numList) {
+  total = 0;
+	for (var i = 0, j = numList.length; i < j; i++) {
+    total = total + Number(numList[i])
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numList) {
+  total = 1
+  for (var i = 0, j = numList.length; i < j; i++) {
+    total = total * Number(numList[i])
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	c = Math.pow(a,b);
+  return c;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	total = 1;
+  for (var i = a; i > 0; i--) {
+    total = total * i;
+  }
+  return total;
 };
 
 // Do not edit below this line
